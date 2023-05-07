87c552b2bcc2e3b9a0fb44cc73a7542a46fbc5cf
Julio Rennan <julio.rennann@gmail.com>
2023-05-07 12:13:59 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/chibata_hub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chibata_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chibata_hub/modules/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: HomaPage(),
+    );
+  }
+}
